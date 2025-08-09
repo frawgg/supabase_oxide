402277ed8e258e5ec0ab615f0693782c7b44c3a2
frawgg <frawggg@proton.me>
2025-08-09 04:05:08 -0400

    add support for Nix dev environments

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  flake-utils.lib.eachDefaultSystem
+  (system:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in
+    with pkgs; {
+      devShells.default = mkShell {
+        buildInputs = [
+          cargo-whatfeatures
+          (rust-bin.stable.latest.default.override {
+            extensions = [ "rust-analyzer" "rust-src" ];
+          })
+        ];
+      };
+    }
+  );
+}
